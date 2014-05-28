5626012dc7915b4606e0dd6f9748964a6b05efca
Francisco Illanes <illanesfco@gmail.com>
2014-05-28 00:14:24 -0600

    Added Assignment 5-2
    Fraction class with overloaded addition operator

---
+++ Assignment 5-2/Assignment5-2/Fraction.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment5_2
+{
+    class Fraction
+    {
+        #region Properties
+        public int Numerator { get; set; }
+        public int Denominator { get; set; }
+        #endregion
+
+        #region Constructors
+        public Fraction()
+        {
+            Numerator = 1;
+            Denominator = 1;
+        }
+
+        public Fraction(int numerator, int denominator)
+        {
+            Numerator = numerator;
+            Denominator = denominator;
+        }
+        #endregion
+
+        #region Methods
+        public static Fraction operator +(Fraction first, Fraction second)
+        {
+            Fraction newFraction = new Fraction(first.Numerator, first.Denominator);
+            newFraction.Numerator = newFraction.Numerator * second.Denominator + second.Numerator * newFraction.Denominator;
+            newFraction.Denominator = newFraction.Denominator * second.Denominator;
+            return newFraction;
+        }
+
+        public override string ToString()
+        {
+            return Numerator.ToString() + "/" + Denominator.ToString();
+        }
+        #endregion
+    }
+}
---
+++ Assignment 5-2/Assignment5-2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment5_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fraction oneHalf = new Fraction(1, 2);
+            Fraction oneQuarter = new Fraction(1, 4);
+            Fraction sumFraction = oneHalf + oneQuarter;
+            Console.WriteLine(oneHalf.ToString() + " + " + oneQuarter.ToString() + " = " + sumFraction.ToString());
+        }
+    }
+}
