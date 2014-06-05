880a71fa8ca540657c48e4c5e96b5736553f4863
Francisco Illanes <illanesfco@gmail.com>
2014-06-05 12:58:26 -0600

    Assignment 8-2
    Use of an extension method on String class (and try/catch)

---
+++ Assignment 8-2/Assignment8-2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment8_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myName = "fran";
+            Console.WriteLine("Name is {0} before", myName);
+            myName = myName.UppercaseFirst();
+            Console.WriteLine("Name is {0} after", myName);
+        }
+    }
+}
---
+++ Assignment 8-2/Assignment8-2/StringExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment8_2
+{
+    static public class StringExtensions
+    {
+        public static string UppercaseFirst(this String s)
+        {
+            // Not the smartest way to validate for null or empty string, but we want to practice try-catch, so...
+            try
+            {
+                char[] charArray = s.ToCharArray();
+                charArray[0] = char.ToUpper(charArray[0]);
+                return new string(charArray);
+            }
+            catch
+            {
+                return string.Empty;
+            }
+
+            /* The try/catch can be easily bypassed by using the following code:
+             *  if (string.isNullOrEmpty(s)
+             *      return string.Empty;
+             */
+        }
+
+    }
+}
